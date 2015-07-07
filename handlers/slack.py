5f0208d39ea34a620708c9f5668fd2efb454ab77
nakashima <pixy2001@gmail.com>
2015-07-07 12:48:04 +0900

    revise config

--- handlers/slack.py
+++ handlers/slack.py
@@ -2,8 +2,9 @@ from urllib.request import urlopen, Request
 import json
 
 class LogicomaTefnut(object):
-    def __init__(self, endpoint):
+    def __init__(self, endpoint, payload={}):
         self._endpoint = endpoint
+        self._payload = payload
 
     def __call__(self, name, weather):
         txt = "begin raining at %s" if weather['rainfall'] != 0.0 \
@@ -12,6 +13,7 @@ class LogicomaTefnut(object):
                 "username": "Logicoma - Tefnut",
                 "text": txt % name,
                 "icon_emoji": ":logicoma:"}
+        payload_obj.update(self._payload)
         payload = json.dumps(payload_obj).encode('utf-8')
         r = Request(self._endpoint, payload, {'Content-Type': 'applicaion/json'})
         return urlopen(r).status == 200
